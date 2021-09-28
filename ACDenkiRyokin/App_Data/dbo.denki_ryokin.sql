ceb028dac00773211f601d39c2b1b0e0bd411e83
栁沼 雅人 <killer_face_93@hotmail.co.jp>
2021-09-28 13:08:44 +0900

    9/28現在の作成物

---
+++ ACDenkiRyokin/App_Data/dbo.denki_ryokin.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[denki_ryokin] (
+    [ManagementID]         NVARCHAR (50) NOT NULL,
+    [ChangeInfomation] NVARCHAR (15) NULL,
+    [Date]         DATE          NULL,
+    [Applicant]        NVARCHAR (50) NULL,
+    [Status]           NVARCHAR (10) NULL,
+    PRIMARY KEY CLUSTERED ([ManagementID] ASC)
+);
+
